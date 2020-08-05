71a6ce6e5eb59e49176f22001f77881e0086aa06
Saryn Mooney <smooney15@my.whitworth.edu>
2020-08-05 09:13:19 -0600

    event class created

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -37,18 +37,7 @@ class FoodTruckTest < Minitest::Test
     food_truck.stock(item1, 25)
     assert_equal 55, food_truck.check_stock(item1)
     food_truck.stock(item2, 12)
-    assert_equal ({item1 => 55, item2 => 12}), food_truck.inventory 
+    assert_equal ({item1 => 55, item2 => 12}), food_truck.inventory
   end
 
 end
-
-
-
-# #=> #<FoodTruck:0x00007f85683152f0...>
-#
-
-#
-#
-# pry(main)> food_truck.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
-# ```
